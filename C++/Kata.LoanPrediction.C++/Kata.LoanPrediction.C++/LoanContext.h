3582af3a24f89d4a280d9b787e2310d1b3c1dad6
Andrew D <retroburst@gmail.com>
2015-09-05 15:48:13 +0900

    Missed files in check in.

--- C++/Kata.LoanPrediction.C++/Kata.LoanPrediction.C++/LoanContext.h
+++ C++/Kata.LoanPrediction.C++/Kata.LoanPrediction.C++/LoanContext.h
@@ -2,7 +2,7 @@
 //  LoanContext.h
 //  Kata.LoanPrediction.C++
 //
-//  Created by Andrew Dowding on 3/09/2015.
+//  Created by a.j.dowding on 3/09/2015.
 //  Copyright (c) 2015 a.j.dowding. All rights reserved.
 //
 
